d5b3e3fb4432b83126e19abe956419568b9cfa9c
barlianutomo <barlian.in@gmail.com>
2024-05-22 17:27:05 +0700

    first commit

---
+++ lib/components/action_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hangman/utilities/constants.dart';
+
+class ActionButton extends StatelessWidget {
+  const ActionButton({super.key, required this.buttonTitle, this.onPress});
+
+  final VoidCallback? onPress;
+  final String buttonTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        foregroundColor: kActionButtonHighlightColor,
+        backgroundColor: kActionButtonColor,
+        elevation: 3.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      onPressed: onPress,
+      child: Text(
+        buttonTitle,
+        style: kActionButtonTextStyle,
+      ),
+    );
+  }
+}
